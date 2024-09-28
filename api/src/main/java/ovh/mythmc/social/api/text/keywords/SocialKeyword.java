fb08f582347747eaa75cffc3b32f5896eb399511
U8092 <uisl8092@gmail.com>
2024-09-28 18:56:12 +0200

    feat: keywords (unused for now)

---
+++ api/src/main/java/ovh/mythmc/social/api/text/keywords/SocialKeyword.java
@@ -0,0 +1,27 @@
+package ovh.mythmc.social.api.text.keywords;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextReplacementConfig;
+import ovh.mythmc.social.api.players.SocialPlayer;
+import ovh.mythmc.social.api.text.SocialParser;
+import ovh.mythmc.social.api.text.filters.SocialFilterLike;
+
+import java.util.regex.Pattern;
+
+
+public abstract class SocialKeyword implements SocialParser, SocialFilterLike {
+
+    public abstract String keyword();
+
+    public abstract String process(SocialPlayer socialPlayer);
+
+    @Override
+    public Component parse(SocialPlayer socialPlayer, Component message) {
+        return message.replaceText(TextReplacementConfig
+                .builder()
+                .match(Pattern.compile("\\[(?i:" + keyword() + "\\b)\\]"))
+                .replacement(process(socialPlayer))
+                .build());
+    }
+
+}
