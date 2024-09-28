5d1d46b48959aa0f3199386e960a8dfe8751385d
U8092 <uisl8092@gmail.com>
2024-09-28 03:32:19 +0200

    fix: make @clickable_nickname return @nickname if /pm is disabled

--- platform-common/src/main/java/ovh/mythmc/social/common/text/placeholders/ClickableNicknamePlaceholder.java
+++ platform-common/src/main/java/ovh/mythmc/social/common/text/placeholders/ClickableNicknamePlaceholder.java
@@ -13,6 +13,10 @@ public final class ClickableNicknamePlaceholder extends SocialPlaceholder {
 
     @Override
     public String process(SocialPlayer player) {
+        // We'll return @nickname if PMs are disabled
+        if (!Social.get().getConfig().getSettings().getCommands().getPrivateMessage().enabled())
+            return "@nickname";
+
         String hoverText = Social.get().getConfig().getSettings().getChat().getClickableNicknameHoverText();
         if (!player.getNickname().equals(player.getPlayer().getName())) {
             hoverText = hoverText + "\n" + Social.get().getConfig().getSettings().getChat().getPlayerAliasWarningHoverText();
