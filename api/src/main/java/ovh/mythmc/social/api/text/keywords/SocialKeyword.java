1fb3688a30ca775d66cd450a53f864cb0eeed0e1
U8092 <uisl8092@gmail.com>
2024-09-29 19:23:08 +0200

    fix: deserialize minimessage in keywords

--- api/src/main/java/ovh/mythmc/social/api/text/keywords/SocialKeyword.java
+++ api/src/main/java/ovh/mythmc/social/api/text/keywords/SocialKeyword.java
@@ -2,6 +2,7 @@ package ovh.mythmc.social.api.text.keywords;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextReplacementConfig;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import ovh.mythmc.social.api.players.SocialPlayer;
 import ovh.mythmc.social.api.text.SocialPlayerInputParser;
 
@@ -15,10 +16,11 @@ public abstract class SocialKeyword implements SocialPlayerInputParser {
 
     @Override
     public Component parse(SocialPlayer socialPlayer, Component message) {
+        Component processedText = MiniMessage.miniMessage().deserialize(process(socialPlayer));
         return message.replaceText(TextReplacementConfig
                 .builder()
                 .match(Pattern.compile("\\[(?i:" + keyword() + "\\b)\\]"))
-                .replacement(process(socialPlayer))
+                .replacement(processedText)
                 .build());
     }
 
