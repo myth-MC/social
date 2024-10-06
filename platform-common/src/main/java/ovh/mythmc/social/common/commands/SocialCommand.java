2623f8b43e1fd7ae994b3fe1b57172adb44cf3fb
U8092 <uisl8092@gmail.com>
2024-10-06 21:27:28 +0200

    feat: /social announcement <id>

--- platform-common/src/main/java/ovh/mythmc/social/common/commands/SocialCommand.java
+++ platform-common/src/main/java/ovh/mythmc/social/common/commands/SocialCommand.java
@@ -3,10 +3,7 @@ package ovh.mythmc.social.common.commands;
 import org.jetbrains.annotations.NotNull;
 import ovh.mythmc.social.api.Social;
 import ovh.mythmc.social.api.players.SocialPlayer;
-import ovh.mythmc.social.common.commands.subcommands.ChannelSubcommand;
-import ovh.mythmc.social.common.commands.subcommands.NicknameSubcommand;
-import ovh.mythmc.social.common.commands.subcommands.ReloadSubcommand;
-import ovh.mythmc.social.common.commands.subcommands.SocialSpySubcommand;
+import ovh.mythmc.social.common.commands.subcommands.*;
 
 import java.util.*;
 
@@ -16,6 +13,7 @@ public abstract class SocialCommand {
 
     public SocialCommand() {
         this.subCommands = new HashMap<>();
+        subCommands.put("announcement", new AnnouncementSubcommand());
         subCommands.put("channel", new ChannelSubcommand());
         subCommands.put("nickname", new NicknameSubcommand());
         subCommands.put("reload", new ReloadSubcommand());
