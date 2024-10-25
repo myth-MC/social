89ed9a8c4c0fe2df71d33b706ac3f020fe04e397
U8092 <uisl8092@gmail.com>
2024-10-25 18:38:56 +0000

    deprecate bootstrap event (will be replaced with gestalt features)

--- api/src/main/java/ovh/mythmc/social/api/events/SocialBootstrapEvent.java
+++ api/src/main/java/ovh/mythmc/social/api/events/SocialBootstrapEvent.java
@@ -6,10 +6,13 @@ import lombok.Setter;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.ApiStatus.ScheduledForRemoval;
 
 @RequiredArgsConstructor
 @Getter
 @Setter
+@Deprecated
+@ScheduledForRemoval
 public class SocialBootstrapEvent extends Event {
 
     private static final @NotNull HandlerList handlers = new HandlerList();
