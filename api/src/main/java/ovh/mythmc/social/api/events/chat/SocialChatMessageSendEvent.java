5ff95b148029968cda5b04c47766924ab83c0dca
U8092 <uisl8092@gmail.com>
2024-11-11 23:17:31 +0000

    fix: SocialChatMessageSendEvent::getParsedRawMessage can produce a loop

--- api/src/main/java/ovh/mythmc/social/api/events/chat/SocialChatMessageSendEvent.java
+++ api/src/main/java/ovh/mythmc/social/api/events/chat/SocialChatMessageSendEvent.java
@@ -3,11 +3,13 @@ package ovh.mythmc.social.api.events.chat;
 import lombok.Getter;
 import lombok.Setter;
 import net.kyori.adventure.text.Component;
+
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.ApiStatus.ScheduledForRemoval;
+
 import ovh.mythmc.social.api.Social;
 import ovh.mythmc.social.api.chat.ChatChannel;
-import ovh.mythmc.social.api.context.SocialParserContext;
 import ovh.mythmc.social.api.players.SocialPlayer;
 
 @Getter
@@ -20,13 +22,19 @@ public class SocialChatMessageSendEvent extends SocialChatMessagePrepareEvent {
         super(sender, chatChannel, message, replyId);
     }
 
+    // TODO: delete this!!
+    // make ChatHistory store contexts instead of events?
+    @Deprecated
+    @ScheduledForRemoval
     public Component getParsedRawMessage() {
+        return Component.text(getRawMessage());
+        /*
         SocialParserContext context = SocialParserContext.builder()
             .socialPlayer(getSender())
             .message(Component.text(getRawMessage()))
             .build();
 
-        return Social.get().getTextProcessor().parsePlayerInput(context);
+        return Social.get().getTextProcessor().parsePlayerInput(context); */
     }
 
     public Integer getId() {
