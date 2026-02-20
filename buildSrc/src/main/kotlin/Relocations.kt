954849ed04231e47194295616c0798e2bed248e8
U8092 <uisl8092@gmail.com>
2026-02-20 12:38:01 +0100

    fix: missing gradle wrapper & relocations

---
+++ buildSrc/src/main/kotlin/Relocations.kt
@@ -0,0 +1,9 @@
+object Relocations {
+    val relocations: Map<String, String> = mapOf(
+        "de.exlll" to "ovh.mythmc.social.libs.de.exlll",
+        "org.snakeyaml" to "ovh.mythmc.social.libs.org.snakeyaml",
+        "com.j256" to "ovh.mythmc.social.libs.com.j256",
+        "org.bstats" to "ovh.mythmc.social.libs.org.bstats",
+        "me.imdanix" to "ovh.mythmc.social.libs.me.imdanix",
+    )
+}
