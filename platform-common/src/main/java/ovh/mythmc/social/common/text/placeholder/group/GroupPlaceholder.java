7780c781c0e3f5e6883e8f8c8a7fa7c37e580e57
U8092 <uisl8092@gmail.com>
2026-02-25 23:05:30 +0100

    fix(platform-common): Replace Mutable with Optional for handling group chat channels in group placeholders

--- platform-common/src/main/java/ovh/mythmc/social/common/text/placeholder/group/GroupPlaceholder.java
+++ platform-common/src/main/java/ovh/mythmc/social/common/text/placeholder/group/GroupPlaceholder.java
@@ -1,10 +1,11 @@
 package ovh.mythmc.social.common.text.placeholder.group;
 
+import java.util.Optional;
+
 import net.kyori.adventure.text.Component;
 import ovh.mythmc.social.api.chat.channel.GroupChatChannel;
 import ovh.mythmc.social.api.context.SocialParserContext;
 import ovh.mythmc.social.api.text.parser.SocialContextualPlaceholder;
-import ovh.mythmc.social.api.util.Mutable;
 
 public final class GroupPlaceholder extends SocialContextualPlaceholder {
 
@@ -20,7 +21,7 @@ public final class GroupPlaceholder extends SocialContextualPlaceholder {
 
     @Override
     public Component get(SocialParserContext context) {
-        final Mutable<GroupChatChannel> groupChannel = context.user().groupChannel();
+        final Optional<GroupChatChannel> groupChannel = context.user().groupChannel();
         if (groupChannel.isEmpty()) {
             return Component.empty();
         }
